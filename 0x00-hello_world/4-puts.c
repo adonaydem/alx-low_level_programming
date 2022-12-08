69c2b7a9351a090525aaf879da99058004995c93
Adonay <adonaydemewez1@gmail.com>
2022-12-07 22:43:55 -0800

    Gh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	/**
+	 * main - function
+	 *
+	 * return- 0
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
