72584bec9393f9fe9350b72d3baec2d9a118b38d
Adonay <adonaydemewez1@gmail.com>
2022-12-07 22:46:33 -0800

    Gh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 int main(void)
 {
 	/**
-	 * main - function
+	 * main - Entry point
 	 *
-	 * return- 0
+	 * Return: Always 0 (Success)
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
