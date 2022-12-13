0dba500e2bc1e5e494013273a4ba885713d1380e
Adonay <adonaydemewez1@gmail.com>
2022-12-13 10:58:04 -0800

    rt

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * _islower - Check description
+ *
+ * Return: 1 or 0
+ */
+int _islower(char test)
+{
+	char i;
+	int g = 0;
+
+	for (i = 'a'; i <= 'z'; ++i)
+		if (test == i)
+			g = 1;
+	return (g);
+}
