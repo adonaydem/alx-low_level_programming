afb95702eb6c07692f3a226739511b0e1a2c4c2b
Adonay <adonaydemewez1@gmail.com>
2022-12-13 11:24:25 -0800

    rt

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,18 @@
 
 /**
  * _islower - Check description
- * @test - An input character
+ * @c - An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
  */
-int _islower(int test)
+int _islower(int c)
 {
 	char i;
 	int g = 0;
 
 	for (i = 'a'; i <= 'z'; ++i)
-		if (test == i)
+		if (c == i)
 			g = 1;
 	return (g);
 }
