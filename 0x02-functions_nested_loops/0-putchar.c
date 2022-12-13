363124868a57379ab1479d5743745f0b2fe49b6f
Adonay <adonaydemewez1@gmail.com>
2022-12-13 10:09:49 -0800

    gh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
-	_putchar("_putchar");
-	return(0);
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+		_putchar (word[i]);
+	_putchar ('\n');
+
+	return (0);
 }
