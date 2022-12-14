a69a5b01f01b4528e12edc712c98089b3045a768
Adonay <adonaydemewez1@gmail.com>
2022-12-14 03:30:10 -0800

    Rt

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - Check description
+ * Description - function returns 1 if
+ * the character is a letter and 0 otherwise
+ * Return: 1 if is alphabet or 0 if otherwise
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (g = 0; g <= 9; i++)
+			_putchar((i * g) + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		_putchar('\n');
+	}
+}
