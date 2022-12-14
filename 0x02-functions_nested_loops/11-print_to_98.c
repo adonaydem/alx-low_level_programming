fb14df537cd1ddfa92b3a0656bc333ee1a2a84c2
Adonay <adonaydemewez1@gmail.com>
2022-12-14 03:08:47 -0800

    Rt

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Check description
+ * @c: Check description
+ * Description - function returns 1 if
+ * the character is a letter and 0 otherwise
+ * Return: 1 if is alphabet or 0 if otherwise
+ */
+void print_to_98(int c)
+{
+	int i;
+
+	if (c >= 98)
+	{
+		for (i = 98; i <= c; i++)
+			printf("%d, ", n);
+	}
+	else if (c < 98)
+	{
+		for (i = c; i <= 98; i++)
+			printf("%d, ", n);
+	}
+}
