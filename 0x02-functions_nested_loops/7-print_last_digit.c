edb3087f03eab26a3b1b33ad61b64b7be524600b
Adonay <adonaydemewez1@gmail.com>
2022-12-13 23:12:51 -0800

    t

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Check description
+ * @c: Check description
+ * Description - function returns the
+ * last digit of a number
+ * Return: The last digit
+ */
+int print_last_digit(int c)
+{
+	int val;
+
+	val = c % 10;
+	_putchar(val);
+	return (val);
+}
