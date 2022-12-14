644762153eea3877709b2c9d71b694315ccc9b22
Adonay <adonaydemewez1@gmail.com>
2022-12-13 23:35:49 -0800

    Rt

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Check description
+ * @c: Check description
+ * @d: Check decription
+ * Description - function returns 1 if
+ * the character is a letter and 0 otherwise
+ * Return: 1 if is alphabet or 0 if otherwise
+ */
+int add(int c, int d)
+{
+	int val;
+	
+	val = c + d;
+	return (val);
+}
