9d7ea067800680dec96bb4c6745aeecfc9543fbe
Adonay <adonaydemewez1@gmail.com>
2022-12-14 07:52:32 -0800

    F

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int thr;
+	int fiv;
+	int sum;
+
+	for (i = 0; i <= 500; i++)
+	{
+		thr = 3 * i;
+		fiv = 5 * i;
+		if (thr > 1024  || fiv > 1024)
+		{
+			break;
+		}
+		else
+		{
+			sum += thr;
+			sum += fiv;
+		}
+	}
