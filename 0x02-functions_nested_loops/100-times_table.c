2b57397c0ce32c400ca78a4323bfbee676f1c147
Adonay <adonaydemewez1@gmail.com>
2022-12-14 07:19:04 -0800

    F

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - Check description
+ * @n: Check description
+ * Description - function returns 1 if
+ * the character is a letter and 0 otherwise
+ * Return: 1 if is alphabet or 0 if otherwise
+ */
+void print_times_table(int n)
+{
+	int i;
+	int g;
+
+	if (n < 0 || n > 15)
+
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (g = 0; g <= n; g++)
+			{
+				if (((i * g) / 10) == 0)
+				{
+					if (g != 0)
+					{
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					_putchar(((i * g) / 10) + '0');
+				}
+				_putchar(((i * g) % 10) + '0');
+				if (g != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 int add(int, int);
 void print_to_98(int);
 void times_table(void);
+void print_times_table(int);
