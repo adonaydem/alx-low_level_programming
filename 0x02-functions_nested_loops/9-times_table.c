9af3b5185fceb0ab9d0ea4627fd25ca94285e0d1
Adonay <adonaydemewez1@gmail.com>
2022-12-14 03:35:11 -0800

    g

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (g = 0; g <= 9; i++)
-			if ((i * g) > 10
+			if ((i * g) >= 10)
 				_putchar(' ');
 			_putchar((i * g) + '0');
 			_putchar(',');
