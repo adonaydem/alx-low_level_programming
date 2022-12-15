965439078cdee32a4316bcd4287c2ab5ce421511
Adonay <adonaydemewez1@gmail.com>
2022-12-15 04:43:15 -0800

    G

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Check description
+ * Description - the function prints numbers
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		 _puthcar(i);
+	}
+	_putchar('\n');
+}
