441e9c57d760c81f4d94af3f9a09aacd97e2b3e0
Adonay <adonaydemewez1@gmail.com>
2022-12-15 05:01:16 -0800

    g

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - Check description
+ * print_most_numbers - Check description
  * Description - the function prints numbers
  * Return: Always 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
