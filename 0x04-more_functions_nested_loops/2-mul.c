1d727136b6de76266d2055b682e47647761a2e92
Adonay <adonaydemewez1@gmail.com>
2022-12-15 04:23:36 -0800

    g

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Check description
+ * @a: Input number
+ * @b: Input number
+ * Description - the function returns 1 if input is digit
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
