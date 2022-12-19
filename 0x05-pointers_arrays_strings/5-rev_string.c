fc9d79c87ce47be388a089daf882fa1b5e1cd330
Adonay <adonaydemewez1@gmail.com>
2022-12-19 11:07:58 -0800

    G

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,19 +3,22 @@
 /**
  * rev_string - Check description
  * @s: String
- * Return: Returns the the length of string
+ * Return: Returns the t
+ * he length of string
  */
 void rev_string(char *s)
 {
 	int i = 0;
+	int b = 0;
 	int c = 0;
-	char tmp[];
+	char *tmp;
 
 	while (s[i] != '\0')
 	{
 		i++;
+		tmp[b] = s[i];
+		b++;
 	}
-	*tmp = *s;
 	while (i)
 	{
 		s[c] = tmp[--i];
