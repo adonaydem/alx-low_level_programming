072995be32596e3e011574bfd57729223643f488
Adonay <adonaydemewez1@gmail.com>
2022-12-19 11:12:02 -0800

    G

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,15 @@
 void rev_string(char *s)
 {
 	int i = 0;
-	int b = 0;
 	int c = 0;
-	char *tmp;
 
 	while (s[i] != '\0')
 	{
 		i++;
-		tmp[b] = s[i];
-		b++;
 	}
 	while (i)
 	{
-		s[c] = tmp[--i];
+		s[c] = s[--i];
 		c++;
 	}
 }
