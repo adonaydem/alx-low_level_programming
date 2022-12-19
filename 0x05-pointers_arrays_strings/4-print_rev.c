583d1e9a5461728bde51117f971ea0280f41c31f
Adonay <adonaydemewez1@gmail.com>
2022-12-19 07:41:53 -0800

    g

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Check description
+ * @s: String
+ * Return: Returns the the length of string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int count = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++
+	}
+	while (s[count] > 0)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+	_putchar('\n');
+}
