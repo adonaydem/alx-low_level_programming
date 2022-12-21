9b7587ea558a04b7333530fbf051a898bbeac6b4
Adonay <adonaydemewez1@gmail.com>
2022-12-21 10:28:05 -0800

    G

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,17 +12,21 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int g = 0;
+	int h = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	while (src[i] != '\0')
+		h++;
 	while (src[g] != '\0' && g < (n -1))
 	{
 		dest[i] = src[g];
 		i++;
 		g++;
 	}
-	dest[i] = src[g];
+	if (h >= n)
+		dest[i] = src[g];
 	return (dest);
 }
