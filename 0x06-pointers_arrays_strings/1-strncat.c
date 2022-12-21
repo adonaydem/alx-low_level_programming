f4cc5c35e67619f6e6bce2e404b439c37da472fe
Adonay <adonaydemewez1@gmail.com>
2022-12-21 10:20:43 -0800

    g

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - Check
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: Nothing
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int g = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[g] != '\0' && g < (n -1))
+	{
+		dest[i] = src[g];
+		i++;
+		g++;
+	}
+	dest[i] = src[g];
+}
